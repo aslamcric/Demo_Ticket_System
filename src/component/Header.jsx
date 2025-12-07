e650901d4d9a562cd2c4768041baca69e336dd1f
aslamcric <mdaslamcric@gmail.com>
2025-12-07 10:06:42 +0600

    Implement navigation with NavLink in Header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "../assets/images/logo.png";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light px-3 container">
       {/* LEFT — Logo */}
-      <a className="navbar-brand d-flex align-items-center" href="/">
+      <NavLink className="navbar-brand d-flex align-items-center" to="/">
         <img src={logo} alt="logo" height="40" />
-      </a>
+      </NavLink>
 
       {/* MOBILE TOGGLER */}
       <button
@@ -26,31 +27,51 @@ const Header = () => {
         id="navbarMenu"
       >
         <ul className="navbar-nav gap-4 px-4 py-2 custom-menu">
-          {/* <ul class="navbar-nav gap-4 px-4 py-2 custom-navbar"> */}
           <li className="nav-item">
-            <a className="nav-link" href="/">
+            <NavLink
+              to="/"
+              end
+              className={({ isActive }) =>
+                "nav-link" + (isActive ? " active-menu" : "")
+              }
+            >
               Home
-            </a>
+            </NavLink>
           </li>
 
           <li className="nav-item">
-            <a className="nav-link" href="/openTicket">
+            <NavLink
+              to="/openTicket"
+              className={({ isActive }) =>
+                "nav-link" + (isActive ? " active-menu" : "")
+              }
+            >
               Open a Ticket
-            </a>
+            </NavLink>
           </li>
 
           <li className="nav-item position-relative">
-            <a className="nav-link active" href="/ticketPage">
+            <NavLink
+              to="/ticketPage"
+              className={({ isActive }) =>
+                "nav-link" + (isActive ? " active-menu" : "")
+              }
+            >
               My Ticket
-            </a>
-            <span className="underline"></span>
+            </NavLink>
           </li>
         </ul>
       </div>
 
       {/* RIGHT — Logout */}
-      <div className="d-none d-lg-block">
+      {/* <div className="d-none d-lg-block">
         <button className="btn px-3 logout-btn">👤 Logout</button>
+      </div> */}
+
+      <div className="d-none d-lg-block">
+        <NavLink to="/login" className="btn px-3 logout-btn">
+          👤 Logout
+        </NavLink>
       </div>
     </nav>
   );
--- src/root/Route.jsx
+++ src/root/Route.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import TicketPage from "../pages/TicketPage";
 import OpenTicket from "../pages/OpenTicket";
+import Login from "../pages/Login";
 
 export const router = createBrowserRouter([
   {
@@ -14,12 +15,16 @@ export const router = createBrowserRouter([
       // },
       {
         path: "/ticketPage",
-        element: <TicketPage></TicketPage>,
+        element: <TicketPage />,
       },
 
       {
         path: "/openTicket",
         element: <OpenTicket />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
       },
     ],
   },
